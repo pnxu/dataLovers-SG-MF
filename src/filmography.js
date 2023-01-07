c41034a4607c47bdf5e86aa26bbf0e9a9481c21e
Marilu Cornelio <katherinacornelio@gmail.com>
2023-01-07 12:52:50 -0500

    haciendo la seccion animation

--- src/filmography.js
+++ src/filmography.js
@@ -1,4 +1,6 @@
+import { filterTitle } from "./data.js";
 import studioGhibli from "./data/ghibli/ghibli.js";
+
 const arrayGhibli = studioGhibli.films;
 // header
 const main = document.querySelector("main");
@@ -13,3 +15,52 @@ main.addEventListener("click", () => {
 footer.addEventListener("click", () => {
   headerInput.checked = false;
 });
+// *****************************************************************
+
+// usando las etiquetas de filmography.html
+const animationContainer = document.querySelector(".animation-container");
+
+const showAbstract = (obj) => {
+  animationContainer.innerHTML = "";
+  animationContainer.innerHTML = `<section class="animation-image">
+           <img
+            src=${obj.poster} alt=${obj.title}> 
+        </section>
+        <section class="animation-abstract">
+          <div class="animation-title">
+            <h1>${obj.title}</h1>
+            <p>${obj.release_date}</p> 
+          </div>
+          <div class="animation-info">
+            <div class="animation-director">
+              <p>DIRECTED BY : </p>
+              <p>${obj.director}</p>
+            </div>
+            <div class="animation-productor">
+              <p>PRODUCED BY : </p>
+              <p>${obj.producer}</p>
+            </div>
+            <div class="animation-description">
+              <p>DESCRIPTION : </p>
+              <p>
+              ${obj.description}
+              </p>
+            </div>
+          </div>
+        </section>`;
+};
+
+// animation
+// usando los titulos dados por films.html
+
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".films-grid__section-title a")) {
+    // localStorage.getItem("titleFilm");
+    localStorage.setItem("titleFilm", e.target.innerHTML);
+  }
+});
+if (location.pathname === "/src/filmography.html") {
+  showAbstract(filterTitle(arrayGhibli, localStorage.getItem("titleFilm")));
+}
+
+//********************************************************************
