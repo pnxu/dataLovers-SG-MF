bf39079f45f42e4ee8078ba9820e55b01a9f6333
francisca <fcaavv@gmail.com>
2023-01-16 11:35:14 -0300

    Updates

--- filmography.js
+++ filmography.js
@@ -329,7 +329,7 @@ document.addEventListener("click", (e) => {
     localStorage.setItem("titleFilm", e.target.innerHTML);
   }
 });
-if (location.pathname === "/dataLovers-SG-MF/filmography.html") {
+if (location.pathname === "/pnxu.github.io/dataLovers-SG-MF/filmography.html") {
   showAbstract(filterTitle(arrayGhibli, localStorage.getItem("titleFilm")));
 
   // **************************************************************
